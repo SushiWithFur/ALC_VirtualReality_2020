85c5346c4714d098234296b5b8f31b43af446d60
Cooper Christensen <cooper.c757@stu.nebo.edu>
2020-01-23 10:31:28 -0700

    Added trees and textures more

---
+++ Virtual_Reality_Project_01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody playerRb;
+    public float moveForce;
+    public float jumpForce;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+       
+        // Moves the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerRb.AddForce(Vector3.forward * moveForce, ForceMode.Impulse);
+        }
+
+
+    }
+}
